712f758d3613dde784073ecca174812085dc3740
Mr.Toot <ottotoot620@gmail.com>
2021-03-28 00:03:14 +0800

    add files

---
+++ src/main/java/com/toot/pojo/User.java
@@ -0,0 +1,12 @@
+package com.toot.pojo;
+
+/**
+ * @author Mr.toot
+ * @version 1.0.0
+ * @ClassName User
+ * @Description TODO
+ * @date 3/28/2021 12:00 AM
+ */
+public class User {
+    private String name;
+}
